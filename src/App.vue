5cbb3d575f7b921e13bb79b9018cc6162da09216
Otavio Augusto <tavioalves@gmail.com>
2020-03-30 22:32:10 -0300

    feat: Add data on App.vue and start to use it inside our employeee component

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app" class="small-container">
     <h1>Employees</h1>
 
-    <employee-table />
+    <employee-table :employees="employees" />
   </div>
 </template>
 
@@ -13,6 +13,27 @@ export default {
   name: "App",
   components: {
     EmployeeTable
+  },
+  data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: "Richard Hendricks",
+          email: "richard@piedpiper.com"
+        },
+        {
+          id: 2,
+          name: "Bertram Gilfoyle",
+          email: "gilfoyle@piedpiper.com"
+        },
+        {
+          id: 3,
+          name: "Dinesh Chugtai",
+          email: "dinesh@piedpiper.com"
+        }
+      ]
+    };
   }
 };
 </script>
