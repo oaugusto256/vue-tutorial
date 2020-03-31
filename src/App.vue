a86d663895b4b2539f63ca27f363968cd3d8aaba
Otavio Augusto <tavioalves@gmail.com>
2020-03-30 22:45:29 -0300

    feat: Call emit event at the parent and create a new method function to handle that

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app" class="small-container">
     <h1>Employees</h1>
 
-    <employee-form />
+    <employee-form @add:employee="addEmployee" />
     <employee-table :employees="employees" />
   </div>
 </template>
@@ -37,6 +37,19 @@ export default {
         }
       ]
     };
+  },
+  methods: {
+    addEmployee(employee) {
+      const lastId =
+        this.employees.length > 0
+          ? this.employees[this.employees.length - 1].id
+          : 0;
+
+      const id = lastId + 1;
+      const newEmployee = { ...employee, id };
+
+      this.employees = [...this.employees, newEmployee];
+    }
   }
 };
 </script>
